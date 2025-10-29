8b19e5bae9e0983e08c286610bc349385a00357e
Mohsen-web-coding <mohsenelrhazi3@gmail.com>
2025-10-29 12:43:28 +0100

    first commit

---
+++ src/test/java/ma/project/supplychainx/SupplyChainXApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.project.supplychainx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplyChainXApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
